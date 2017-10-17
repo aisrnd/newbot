55112a80742593b1585d4a0331dd436734244ccc
aisrnd <aisamrm2014@gmail.com>
2017-10-17 14:00:42 +0700

    change file location

--- pushline.php
+++ pushline.php
@@ -1,13 +1,22 @@
 <?php
 
-require_once('./LINEBot.php');
+//require_once('./LINEBot.php');
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('721szhlnKcqiywE45a+x50db4xAn9thVwe9YFKesQRV0dd06rwMsknLaCk+toRzbvpKyIUgBNMnWWw0OYDcoonrhR+XZH8Jk/eV5irg2qvt1bJ1L/RRHX2fAErs+f6gZ9YrkIH6KV3QYcVadV6CawwdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'bf8c23763d219001b1966809f1d3d7b8
-']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello, bot testing');
-$response = $bot->pushMessage('U7fc79408bcc23bd23ca455670086f464', $textMessageBuilder);
+$channel_access_token = '721szhlnKcqiywE45a+x50db4xAn9thVwe9YFKesQRV0dd06rwMsknLaCk+toRzbvpKyIUgBNMnWWw0OYDcoonrhR+XZH8Jk/eV5irg2qvt1bJ1L/RRHX2fAErs+f6gZ9YrkIH6KV3QYcVadV6CawwdB04t89/1O/w1cDnyilFU=';
+
+$channel_secret = 'bf8c23763d219001b1966809f1d3d7b8';
+
+$message2send = 'Hello, bot testing';
+
+$dest_Ids = 'U7fc79408bcc23bd23ca455670086f464';
+
+$httpClient = new \LINEBot\HTTPClient\CurlHTTPClient($channel_access_token);
+$bot = new \LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+
+$textMessageBuilder = new \LINEBot\MessageBuilder\TextMessageBuilder($message2send);
+$response = $bot->pushMessage($dest_Ids, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 ?>
